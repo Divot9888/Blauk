0581e55947cb371720be04175f144821576e0474
CrimXD <TristanAHead@Outlook.com>
2026-03-03 20:42:58 -0800

    Add layer
    Top layer is now different material

--- Assets/TerrainGen/map.gd
+++ Assets/TerrainGen/map.gd
@@ -10,5 +10,11 @@ func _ready() -> void:
 func generate_chunk(cx: int, cz: int) -> void:
 	for x in range(cx*8, (cx+1)*8):
 		for z in range(cz*8, (cz+1)*8):
-			for y in range(floor(max_height / 2.0)):
-				set_cell_item(Vector3i(x, y, z), 1)
+			for y in range(0, floor(max_height / 2.0) + 1):
+				set_cell(Vector3i(x, y, z))
+
+func set_cell(p: Vector3i) -> void:
+	var item := 1
+	if p.y == floor(max_height / 2.0):
+		item = 2
+	set_cell_item(p, item)
