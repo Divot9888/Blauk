8819b1fa6a8bc2deaaa931985bd51690d635ed0d
CrimXD <TristanAHead@Outlook.com>
2026-03-03 20:32:43 -0800

    Update map.gd

--- Assets/TerrainGen/map.gd
+++ Assets/TerrainGen/map.gd
@@ -1,5 +1,7 @@
 extends GridMap
 
+const max_height = 32
+
 func _ready() -> void:
 	for x in range(0, 1):
 		for z in range(0, 1):
@@ -8,5 +10,5 @@ func _ready() -> void:
 func generate_chunk(cx: int, cz: int) -> void:
 	for x in range(cx*8, (cx+1)*8):
 		for z in range(cz*8, (cz+1)*8):
-			for y in range(32):
+			for y in range(floor(max_height / 2.0)):
 				set_cell_item(Vector3i(x, y, z), 1)
