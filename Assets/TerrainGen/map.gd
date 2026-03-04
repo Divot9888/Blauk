64d2b09642df3340dfb50defb12e41e904bf6d71
CrimXD <TristanAHead@Outlook.com>
2026-03-03 20:09:27 -0800

    Godot Update
    - Updated Godot to 4.6.1 Mono
    - Added shading
    - Limited world back to one chunk

--- Assets/TerrainGen/map.gd
+++ Assets/TerrainGen/map.gd
@@ -1,12 +1,12 @@
 extends GridMap
 
 func _ready() -> void:
-	for x in range(0, 8):
-		for z in range(0, 8):
+	for x in range(0, 1):
+		for z in range(0, 1):
 			generate_chunk(x, z)
 
 func generate_chunk(cx: int, cz: int) -> void:
 	for x in range(cx*8, (cx+1)*8):
 		for z in range(cz*8, (cz+1)*8):
 			for y in range(32):
-				set_cell_item(Vector3i(x, y, z), 0)
+				set_cell_item(Vector3i(x, y, z), 1)
