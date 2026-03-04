e66acfd476a52da60946c348161cf292531663fb
CrimXD <TristanAHead@Outlook.com>
2026-03-03 19:04:45 -0800

    Added chunk support
    - Added chunk support
    - Camera FoV is now 90

--- Assets/TerrainGen/map.gd
+++ Assets/TerrainGen/map.gd
@@ -1,10 +1,10 @@
 extends GridMap
 
 func _ready() -> void:
-	generate_map()
+	generate_chunk(0, 0)
 
-func generate_map() -> void:
-	for x in range(8):
-		for z in range(8):
+func generate_chunk(cx: int, cz: int) -> void:
+	for x in range(cx*8, (cx+1)*8):
+		for z in range(cz*8, (cz+1)*8):
 			for y in range(32):
 				set_cell_item(Vector3i(x, y, z), 0)
